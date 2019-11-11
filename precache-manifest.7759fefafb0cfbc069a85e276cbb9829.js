2ed550bf87c31513c0905b3f646e7318cb9af63a
Lucas Lovato <lucas.lovato@gmail.com>
2019-11-11 11:17:59 -0300

    Updates

---
+++ precache-manifest.7759fefafb0cfbc069a85e276cbb9829.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d52ab8209f15b60df330",
+    "url": "/teleprompter/static/js/runtime~main.8ca23ce8.js"
+  },
+  {
+    "revision": "59c88ca4714ae00c92f2",
+    "url": "/teleprompter/static/js/main.99413bd0.chunk.js"
+  },
+  {
+    "revision": "97a8e71ebc37b4758c89",
+    "url": "/teleprompter/static/js/2.e7c9786d.chunk.js"
+  },
+  {
+    "revision": "59c88ca4714ae00c92f2",
+    "url": "/teleprompter/static/css/main.5ca41137.chunk.css"
+  },
+  {
+    "revision": "9033f69e2e04ba9a55b2efdd08ccf13b",
+    "url": "/teleprompter/index.html"
+  }
+];
